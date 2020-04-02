a0b078f655f381d6e7ba6ee932dc94d1e9e89d48
7777ty <568318359@qq.com>
2020-04-02 22:50:23 +0800

    deploy

---
+++ precache-manifest.c174d51b3a7baa3396bcdc49fc0dbce5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c794ca96a6f74ae5b23d",
+    "url": "/MyBook-website/css/app.ac2d3233.css"
+  },
+  {
+    "revision": "dc547e1dab3dcaa1fde7",
+    "url": "/MyBook-website/css/chunk-vendors.6dc57fe0.css"
+  },
+  {
+    "revision": "b226c18a7b241bd0df2abab22c586d68",
+    "url": "/MyBook-website/index.html"
+  },
+  {
+    "revision": "c794ca96a6f74ae5b23d",
+    "url": "/MyBook-website/js/app.5d9537cf.js"
+  },
+  {
+    "revision": "dc547e1dab3dcaa1fde7",
+    "url": "/MyBook-website/js/chunk-vendors.1e88aff4.js"
+  },
+  {
+    "revision": "5df4b4b6e37ce58e9a65ef488ebe9767",
+    "url": "/MyBook-website/manifest.json"
+  },
+  {
+    "revision": "e628cb34d88b8e336da79d5e7c6506e9",
+    "url": "/MyBook-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MyBook-website/robots.txt"
+  }
+]);
